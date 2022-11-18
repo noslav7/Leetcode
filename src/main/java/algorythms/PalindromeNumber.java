9f91d5d6464999e2445d1488778c77fb22cb523f
noslav7 <noslav7@gmail.com>
2022-11-18 09:26:34 +0500

    first three tasks

---
+++ src/main/java/algorythms/PalindromeNumber.java
@@ -0,0 +1,20 @@
+package algorythms;
+
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        int remainder, sum = 0, temp;
+        temp = x;
+
+        while (x > 0) {
+            remainder = x % 10;
+            sum = sum * 10 + remainder;
+            x = x / 10;
+        }
+
+        if (temp == sum) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
