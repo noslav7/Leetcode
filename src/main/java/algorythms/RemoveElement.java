419493f1aa420988744bc19094cb709946f0ac44
noslav7 <noslav7@gmail.com>
2022-11-23 10:19:37 +0500

    RemoveElement

--- src/main/java/algorythms/RemoveElement.java
+++ src/main/java/algorythms/RemoveElement.java
@@ -1,7 +1,16 @@
 package algorythms;
 
+import java.util.Arrays;
+
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
-
+        int insertIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[insertIndex] = nums[i];
+                insertIndex++;
+            }
+        }
+        return insertIndex;
     }
 }
