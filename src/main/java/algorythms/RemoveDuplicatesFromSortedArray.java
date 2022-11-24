86cf197d49ee173734f233e0cc59c7a459a03baa
noslav7 <noslav7@gmail.com>
2022-11-24 17:03:31 +0500

    RemoveDuplicatesFromSortedArray

--- src/main/java/algorythms/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/algorythms/RemoveDuplicatesFromSortedArray.java
@@ -4,10 +4,10 @@ public class RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums) {
         int insertIndex = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i - 1] != nums[i]) {
-                nums[insertIndex] = nums[i];
-                insertIndex++;
-            }
+                if (nums[i - 1] != nums[i]) {
+                    nums[insertIndex] = nums[i];
+                    insertIndex++;
+                }
         }
         return insertIndex;
     }
