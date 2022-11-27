68a4a41630060971d628757a9e4bfa0f950c599d
noslav7 <noslav7@gmail.com>
2022-11-27 19:53:54 +0500

    MajorityElement

---
+++ src/main/java/algorythms/MajorityElement.java
@@ -0,0 +1,21 @@
+package algorythms;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int majorityElement = 0;
+        int majorityCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            if (count > majorityCount) {
+                majorityCount = count;
+                majorityElement = nums[i];
+            }
+        }
+        return majorityElement;
+    }
+}
