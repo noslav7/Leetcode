3db6958a853c94360fa7b835ceea5afa4ca174b2
noslav7 <noslav7@gmail.com>
2022-12-07 19:58:31 +0500

    AddDigits

---
+++ src/main/java/algorythms/AddDigits.java
@@ -0,0 +1,22 @@
+package algorythms;
+
+public class AddDigits {
+    public static int addDigits(int num) {
+        int sum = 0;
+        while (num > 9) {
+            sum = 0;
+            while (num != 0) {
+                int lastDigit = num % 10;
+                sum += lastDigit;
+                num /= 10;
+            }
+            num = sum;
+        }
+        System.out.println(num);
+        return num;
+    }
+
+    public static void main(String[] args) {
+        addDigits(0);
+    }
+}
