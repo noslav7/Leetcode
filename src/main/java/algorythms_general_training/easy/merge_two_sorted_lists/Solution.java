860290c939570d080f5db814cb6a50afc824c216
noslav7 <noslav7@gmail.com>
2022-12-16 19:47:33 +0500

    MergeTwoSortedLists

--- src/main/java/algorythms_general_training/easy/merge_two_sorted_lists/Solution.java
+++ src/main/java/algorythms_general_training/easy/merge_two_sorted_lists/Solution.java
@@ -1,38 +1,48 @@
 package algorythms_general_training.easy.merge_two_sorted_lists;
 
+import org.w3c.dom.Node;
+
 public class Solution {
 
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode result = new ListNode(0);
-        int count = 0;
-        if (list1 == null && list2 == null) {
-            return null;
-        } else if (list1 == null) {
-            return list2;
-        } else if (list2 == null) {
-            return list1;
-        }
+         /* a dummy first node to
+           hang the result on */
+        ListNode dummyNode = new ListNode(0);
 
-        if (list1.val == list2.val) {
-            result.val = list1.val;
+        /* tail points to the
+        last result node */
+        ListNode tail = dummyNode;
+        while (true) {
+
+            /* if either list runs out,
+            use the other list */
+            if (list1 == null) {
+                tail.next = list2;
+                break;
+            }
+            if (list2 == null) {
+                tail.next = list1;
+                break;
+            }
+
+            /* Compare the data of the two
+            lists whichever lists' data is
+            smaller, append it into tail and
+            advance the head to the next Node
+            */
+            if (list1.val <= list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+
+            /* Advance the tail */
+            tail = tail.next;
         }
-        if (list1.val < list2.val && count == 0) {
-            result.val = list1.val;
-            count++;
-        }
-        if (list1.val < list2.val && count == 1) {
-            result.val = list2.val;
-            count = 0;
-        }
-        if (list1.val > list2.val && count == 0) {
-            result.val = list2.val;
-            count++;
-        }
-        if (list1.val > list2.val && count == 1) {
-            result.val = list2.val;
-            count = 0;
-        }
-        return result;
+        return dummyNode.next;
     }
 }
